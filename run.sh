9b6b793bc848095b220a7625747b957ef3397dbc
Erjan Gavalji <erjan.gavalji@gmail.com>
2015-05-07 15:11:46 +0300

    Make a one-liner-run

--- run.sh
+++ run.sh
@@ -1,7 +1,15 @@
 #! /bin/bash
 
+gitresult=$( { git status | grep "fatal: Not a git repository" > /dev/null; } 2>&1 )
+if [[ ! -d "./sample-cuteness" && "${gitresult}" != "" ]]; then
+# Cloning to default dir
+    echo "=> Downloading sample-cuteness from git"
+    printf "\r=> "
+    command git clone "git@github.com:NativeScript/sample-cuteness.git"
+    cd sample-cuteness
+fi
 
-npmresult=$(npm list-g | grep 'nativescript@');
+npmresult=$(npm list -g | grep 'nativescript@');
 if [[ "$npmresult" == "" ]]; then
     npm install -g nativescript
 fi
